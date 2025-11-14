9c9061f5aebec4b3a20e22946acc28086651a51a
akshaywritescode <teamakshayyadav@gmail.com>
2025-11-14 13:17:04 +0530

    add logo nav themeswitcher on header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,58 @@
+import Separator from "./Separator";
+import ThemeSwitcher from "./ThemeSwitcher";
+
 export default function Header() {
     return (
-        <header className="h-[65px] border-b border-b-white/10 w-full flex items-center px-10">
-            {/* Logo */}
-            <h1 className="text-3xl font-bold mr-10">RangoliUI</h1>
-            {/* Navigation Menu */}
-            <nav>
-                <ul className="flex items-center gap-10 text-sm font-medium">
-                    <li>
-                        <a
-                            href="#"
-                            className="p-2 rounded-md hover:bg-accent hover:text-accent-foreground"
-                        >
-                            Get Started
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header className="h-[65px]  w-full flex items-center justify-between px-10">
+                {/* Left Side */}
+                <div className="flex items-center">
+                    {/* Logo */}
+                    <h1 className="text-3xl font-medium mr-10">RangoliUI</h1>
+                    {/* Navigation Menu */}
+                    <nav>
+                        <ul className="flex items-center gap-7 text-sm font-medium">
+                            <li>
+                                <a
+                                    href="#"
+                                    className="p-2 font-medium rounded-md hover:bg-accent hover:text-accent-foreground"
+                                >
+                                    Get Started
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="p-2 font-medium rounded-md hover:bg-accent hover:text-accent-foreground"
+                                >
+                                    Docs
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="p-2 font-medium rounded-md hover:bg-accent hover:text-accent-foreground"
+                                >
+                                    Contribute
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="p-2 font-medium rounded-md hover:bg-accent hover:text-accent-foreground"
+                                >
+                                    Donate
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                {/* Right Side */}
+                <div>
+                    <ThemeSwitcher defaultValue="dark" />
+                </div>
+            </header>
+            <Separator />
+        </>
     )
 }
